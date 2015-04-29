39f81a1f294563ce3ea0c0b012303a7381578473
samuraix <ivodwvio@mail.bg>
2015-04-29 18:36:37 +0300

    Initial commit

---
+++ src/com/zetcode/program0001/Application.java
@@ -0,0 +1,13 @@
+package com.zetcode.program0001;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/**
+ * Created by samuraix on 29/04/15.
+ */
+public class Application extends JFrame {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/zetcode/program0001/Board.java
@@ -0,0 +1,9 @@
+package com.zetcode.program0001;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+
+    public Board() {}
+
+}
